dc1e3c50b4a494e0807e5a80702eeeac55d101bd
KamauNewtonNgatia <kamaunewton315@gmail.com>
2024-11-25 08:07:38 +0300

    created services section and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-
+import ServicesSection from "./components/ServicesSection";
+import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
       <>
         <Header />
         <Hero/>
+        <ServicesSection/>
+        <Footer/>
       </>
     </BrowserRouter>
   );
